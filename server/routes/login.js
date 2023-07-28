957a643ef83f8e1e452691fa9870e5f2b03763e7
Lester Marroquin <lester.marroquin96@gmail.com>
2023-07-27 23:43:58 -0600

    Google Sing-In

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,62 +1,138 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const Usuario = require('../models/usuario');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const Usuario = require("../models/usuario");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
+app.post("/login", function (req, res) {
+  let body = req.body;
+  Usuario.findOne({ email: body.email })
+    .then((usuarioDB) => {
+      if (!usuarioDB) {
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: "(Usuario) o contraseña incorrectos",
+          },
+        });
+      }
 
-app.post('/login', function (req, res) {
+      if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: "Usuario o (contraseña) incorrectos",
+          },
+        });
+      }
 
-    let body = req.body;
-    Usuario.findOne({email: body.email})
-    .then( usuarioDB => {
+      //El metodo jwt.sign recibe como parametros la data, en esta caso
+      //el usuario, una palabra 'secret' que debe de cambiarse en producción
+      //el tiempo de expiración que indica:
+      //60 segundos * 60 minutos * 24 horas * 30 días
+      let token = jwt.sign(
+        {
+          usuario: usuarioDB,
+        },
+        process.env.SEED,
+        { expiresIn: process.env.CADUCIDAD_TOKEN }
+      );
 
-        if (!usuarioDB) {
-            return res.status(400).json({
-                ok: false,
-                err: { 
-                    message: '(Usuario) o contraseña incorrectos'
-                }
-            });
-        }
-
-        if (!bcrypt.compareSync(body.password, usuarioDB.password)){
-            return res.status(400).json({
-                ok: false,
-                err: { 
-                    message: 'Usuario o (contraseña) incorrectos'
-                }
-            });
-        }
-
-        //El metodo jwt.sign recibe como parametros la data, en esta caso
-        //el usuario, una palabra 'secret' que debe de cambiarse en producción
-        //el tiempo de expiración que indica:
-        //60 segundos * 60 minutos * 24 horas * 30 días
-        let token =  jwt.sign({
-            usuario: usuarioDB
-        }, process.env.SEED, {expiresIn: process.env.CADUCIDAD_TOKEN })
-
-        res.json({
-            ok: true,
-            usuario: usuarioDB,
-            token
-        })
-
+      res.json({
+        ok: true,
+        usuario: usuarioDB,
+        token,
+      });
     })
-    .catch(err => {
-        res.status(500).json({
-            ok: false,
-            err
-        })
-    })
-
+    .catch((err) => {
+      res.status(500).json({
+        ok: false,
+        err,
+      });
+    });
 });
 
+//Configuraciones de Google
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
 
+  return {
+    nombre: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true,
+  };
+}
 
+app.post("/google", async (req, res) => {
+  let token = req.body.idtoken;
 
+  try {
+    let googleUser = await verify(token);
 
+    let usuarioDB = await Usuario.findOne({ email: googleUser.email });
+    if (usuarioDB) {
+      if (usuarioDB.google === false) {
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: "Usuario ya creado, debe de iniciar sesión",
+          }
+        });
+      } else {
+        let token = jwt.sign(
+          {
+            usuario: usuarioDB,
+          },
+          process.env.SEED,
+          { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
 
+        return res.json({
+          ok: true,
+          usuario: usuarioDB,
+          token,
+        });
+      }
+    } else {
+      //Si el usuario no existe en nuestra BD, se creara
+      let usuario = new Usuario();
+      usuario.nombre = googleUser.nombre;
+      usuario.email = googleUser.email;
+      usuario.img = googleUser.img;
+      usuario.google = true;
+      usuario.password = "=)";
+
+      usuarioDB = await usuario.save();
+
+      let token = jwt.sign(
+        {
+          usuario: usuarioDB,
+        },
+        process.env.SEED,
+        { expiresIn: process.env.CADUCIDAD_TOKEN }
+      );
+
+      return res.json({
+        ok: true,
+        usuario: usuarioDB,
+        token,
+      });
+    }
+  } catch (e) {
+    return res.status(403).json({
+      ok: false,
+      err: e,
+    });
+  }
+});
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//Configuración global de rutas
+// Habilitar la carpeta public
+app.use(express.static( path.resolve(__dirname, '../public')));
+
+// Configuración global de rutas
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true })
